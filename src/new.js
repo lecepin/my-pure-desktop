58b0ca97419d0cfcf68a803c178e6f7242a130d4
lecepin <383810086@qq.com>
2022-03-25 14:44:08 +0800

    feat: done

---
+++ src/new.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import New from "./new/index";
+
+const app = document.getElementById("app");
+
+ReactDOM.render(<New />, app);
---
+++ src/new/index.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import { useMachine } from "@xstate/react";
+import fsm from "./fsm";
+
+import "./index.less";
+
+export default () => {
+  const [state, send] = useMachine(fsm);
+  const {
+    context: { timeText, currentBg, bgList, dateText, dayText },
+  } = state;
+
+  useEffect(() => {
+    function keydown(e) {
+      send(e);
+    }
+
+    window.addEventListener("keydown", keydown);
+
+    return () => {
+      window.removeEventListener("keydown", keydown);
+    };
+  }, []);
+
+  return (
+    <div
+      className="new"
+      style={{
+        backgroundImage: `url(${bgList[currentBg]})`,
+      }}
+    >
+      <div className="new-time">
+        {timeText}
+        <div className="new-time-date">{dateText}</div>
+        <div className="new-time-day">Week {dayText}</div>
+      </div>
+    </div>
+  );
+};
